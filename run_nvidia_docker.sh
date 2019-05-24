a2eb72f186e98f79c827bfc116e8d9998b4b55eb
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-05-24 14:32:15 +0900

    update controller

--- run_nvidia_docker.sh
+++ run_nvidia_docker.sh
@@ -18,4 +18,5 @@ nvidia-docker run -it --rm \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --net="host" \
   --volume="$SCRIPT_DIR/:/root/catkin_ws/src/sq2_ccv_description/" \
-  $IMAGE_NAME
+  $IMAGE_NAME \
+  bash -c "cd /root/catkin_ws/src && git clone https://github.com/amslabtech/diff_drive_steering_controller -b devel && cd /root/catkin_ws && catkin_make && bash"
