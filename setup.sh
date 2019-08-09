d58af40d5b00f359d898aa7985d3db673430980a
RIckyBan <ricky1231@g.ecc.u-tokyo.ac.jp>
2019-08-09 15:16:08 +0900

    add model

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 cd `dirname $0`
 
 . /usr/share/gazebo/setup.sh
-. ~/ws/install/setup.bash
+. ~/ccv_ws/install/setup.bash
+export GAZEBO_MODEL_PATH=~/ccv_ws/src/sq2_ccv_description/models:${GAZEBO_MODEL_PATH}
 export GAZEBO_RESOURCE_PATH=~/ccv_ws/src/sq2_ccv_description/worlds:${GAZEBO_RESOURCE_PATH}
